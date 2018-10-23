c8481e2ace036dd54e226624e08cd4df31c08039
유영재 <asamaru@asamaru.net>
2018-10-23 09:21:33 +0900

    update

--- res/js/ad.slots.js
+++ res/js/ad.slots.js
@@ -9,96 +9,96 @@ var adslot = (ADSlots = window.ADSlots || []);
 
 	if (!isMobile) {
 		adslot.push({	// 블로그 우측
-			'target': '#cSide',
-			'slots': [{
-				'position': 'top',
-				'type': 'adsense',
-				'client': 'ca-pub-8163803188491150',
-				'slot': '1934918622',
-				'format': 'auto',
-				'style': 'width:300px; min-height:50px; margin-bottom:40px;'
+			target: '#cSide',
+			slots: [{
+				position: 'top',
+				type: 'adsense',
+				client: 'ca-pub-8163803188491150',
+				slot: '1934918622',
+				format: 'auto',
+				style: 'width:300px; min-height:50px; margin-bottom:40px;'
 			}]
 		});
 	}
 
 	adslot.push({	// 블로그 하단
-		'target': '.dMainBody',
-		'slots': [{
-			'position': 'bottom',
-			'type': 'adsense',
-			'client': 'ca-pub-8163803188491150',
-			'slot': '9980888623',
-			'format': 'auto',
-			'style': 'padding-top:30px; clear:both; margin:0 auto;' + (isMini ? 'width:300px; height:600px;' : '')
+		target: '.dMainBody',
+		slots: [{
+			position: 'bottom',
+			type: 'adsense',
+			client: 'ca-pub-8163803188491150',
+			slot: '9980888623',
+			format: 'auto',
+			style: 'padding-top:30px; clear:both; margin:0 auto;' + (isMini ? 'width:300px; height:600px;' : '')
 		}]
 	});
 
 	adslot.push({
-		'target': '#cArticle',
-		'slots': [{// 본문 하단
-			'position': 'bottom',
-			'type': 'adsense',
-			'client': 'ca-pub-8163803188491150',
-			'slot': '7841851420',
-			'format': 'auto',
-			'style': 'margin-top:30px;'
+		target: '#cArticle',
+		slots: [{// 본문 하단
+			position: 'bottom',
+			type: 'adsense',
+			client: 'ca-pub-8163803188491150',
+			slot: '7841851420',
+			format: 'auto',
+			style: 'margin-top:30px;'
 		}, {// 본문 일치하는 콘텐츠
-			'position': 'bottom',
-			'type': 'adsense',
-			'client': 'ca-pub-8163803188491150',
-			'slot': '7054841029',
-			'format': 'autorelaxed',
-			'style': 'margin:30px auto;'
+			position: 'bottom',
+			type: 'adsense',
+			client: 'ca-pub-8163803188491150',
+			slot: '7054841029',
+			format: 'autorelaxed',
+			style: 'margin:30px auto;'
 		}]
 	});
 
 	adslot.push({	// archives index 화면
-		'target': '#cArchiveIndex',
-		'slots': [{	// archive 목록 중간
-			'position': {
-				'selector': 'h3',
-				'index': -2
+		target: '#cArchiveIndex',
+		slots: [{	// archive 목록 중간
+			position: {
+				selector: 'h3',
+				index: -2
 			},
-			'type': 'adsense',
-			'client': 'ca-pub-8163803188491150',
-			'slot': '6221779423',
-			'format': 'auto',
-			'style': 'margin:15px 0;'
+			type: 'adsense',
+			client: 'ca-pub-8163803188491150',
+			slot: '6221779423',
+			format: 'auto',
+			style: 'margin:15px 0;'
 		}, {	// archive 목록 중간 아래
-			'position': {
-				'selector': 'h3',
-				'index': -3
+			position: {
+				selector: 'h3',
+				index: -3
 			},
-			'type': 'adsense',
-			'client': 'ca-pub-8163803188491150',
-			'slot': '7698512627',
-			'format': 'auto',
-			'style': 'margin:15px 0;'
+			type: 'adsense',
+			client: 'ca-pub-8163803188491150',
+			slot: '7698512627',
+			format: 'auto',
+			style: 'margin:15px 0;'
 		}]
 	});
 
 	adslot.push({	// 블로그 글 목록
-		'target': 'ul.dArticles',
-		'slots': [{	// 블로그 목록 중간
-			'position': {
-				'selector': 'li.hentry',
-				'index': 3
+		target: 'ul.dArticles',
+		slots: [{	// 블로그 목록 중간
+			position: {
+				selector: 'li.hentry',
+				index: 3
 			},
-			'type': 'adsense',
-			'client': 'ca-pub-8163803188491150',
-			'slot': '6221779423',
-			'format': 'auto',
-			'style': 'margin:15px 0;'
+			type: 'adsense',
+			client: 'ca-pub-8163803188491150',
+			slot: '6221779423',
+			format: 'auto',
+			style: 'margin:15px 0;'
 		}, {	// 블로그 목록 중간 아래
-			'position': {
-				'selector': 'li.hentry',
-				'index': 10
+			position: {
+				selector: 'li.hentry',
+				index: 10
 			},
-			'type': 'adsense',
-			'client': 'ca-pub-8163803188491150',
-			'slot': '7698512627',
-			'format': 'auto',
-			'style': 'margin:15px 0;'
+			type: 'adsense',
+			client: 'ca-pub-8163803188491150',
+			slot: '7698512627',
+			format: 'auto',
+			style: 'margin:15px 0;'
 		}]
 	});
 

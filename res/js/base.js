32b3a6b0c4de462ec54d1aa4e3100291b2f7cf14
유영재 <asamaru@asamaru.net>
2018-11-15 09:35:09 +0900

    update

--- res/js/base.js
+++
@@ -1,5 +0,0 @@
-(function () {
-	'use strict';
-
-	document.write('<' + 'script async class="g-ad-slots" type="text/javascript" src="/res/js/ad.slots.js?t=' + ((new Date()).toISOString().slice(0, 14).replace(/[- :T]/ig, "")) + '"><' + '/script>');
-})();
